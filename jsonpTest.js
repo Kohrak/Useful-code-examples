f188c1a1b1f924279d4f95dd806e47701b39cdf7
Kohrak <gabrielgt95@gmail.com>
2018-01-17 19:34:55 +0000

    start

---
+++ jsonpTest.js
@@ -0,0 +1,13 @@
+function jsonpRequest(){
+  var tag = document.createElement("script");
+  tag.src = "url &jsonp=mycallback";
+  tag.id = "jsonpCall"
+  document.getElementsByTagName("head")[0].appendChild(tag);
+}
+
+function mycallback(data) {
+  console.log(data.quoteText);
+  document.getElementsByTagName("head")[0].removeChild(document.getElementById("jsonpCall"));
+}
+
+jsonpp();
