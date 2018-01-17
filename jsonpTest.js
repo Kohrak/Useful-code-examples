a28f24051029926140b2399760b7645bb85bb16e
Julio <jfuenmayor96@users.noreply.github.com>
2018-01-17 16:52:55 -0400

    add some documentation skeletons
    You might also add a brief explanation of how to run/execute your code.

--- jsonpTest.js
+++ jsonpTest.js
@@ -1,3 +1,18 @@
+/*
+*  Module/file description
+*  
+*  @author XXXXX
+*  @since 
+*/
+
+
+/*
+*  Brief explanation of the function/method
+*
+*  @param X description
+*  @param Y descrption
+*  @return (type of the return value, none, nothing, brief description of the returned value, etc)
+*/
 function jsonpRequest(){
   var tag = document.createElement("script");
   tag.src = "url &jsonp=mycallback";
@@ -5,9 +20,18 @@ function jsonpRequest(){
   document.getElementsByTagName("head")[0].appendChild(tag);
 }
 
+
+/*
+*  Brief explanation of the function/method
+*
+*  @param X description
+*  @param Y descrption
+*  @return (type of the return value, none, nothing, brief description of the returned value, etc)
+*/
 function mycallback(data) {
   console.log(data.quoteText);
   document.getElementsByTagName("head")[0].removeChild(document.getElementById("jsonpCall"));
 }
 
+/* Why do you call this method here? */
 jsonpp();
